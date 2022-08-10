c8a1cf17b8537b80191e2fbaa9df61a090e8090d
jlardouin <jlardouin@gmail.com>
2022-08-10 15:38:18 +0000

    Comit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "eu-west-0"
+  description = "Region endoint to be refered"
+}
+variable "domain_name" {
+  default = "OCB0001661"
+  description = "domain name to be refered"
+}
+variable "tenant_name" {
+  default = "eu-west-0_jla"
+  description = "tenant name of the domain to be refered"
+}
