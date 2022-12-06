c05dc4d2394ff6fc034f3803fc57e29d55385595
jlardouin <jlardouin@gmail.com>
2022-12-06 12:26:09 +0000

    New vommit

--- main.tf
+++ main.tf
@@ -48,6 +48,50 @@ resource "flexibleengine_networking_router_interface_v2" "back_router_interface"
   subnet_id = flexibleengine_networking_subnet_v2.back_subnet.id
 }
 
+resource "time_sleep" "wait_for_vpc" {
+  create_duration = "30s"
+  depends_on = [flexibleengine_vpc_v1.vpc]
+}
+
+#Create an Elastic IP for NATGW
+resource "flexibleengine_vpc_eip_v1" "eip_natgw" {
+  publicip {
+    type = "5_bgp"
+  }
+  bandwidth {
+    name        = "${var.project}-NATGW-EIP-${random_string.id.result}"
+    size        = 8
+    share_type  = "PER"
+    charge_mode = "traffic"
+  }
+}
+
+#Create NAT GW
+resource "flexibleengine_nat_gateway_v2" "nat_1" {
+  depends_on = [time_sleep.wait_for_vpc]
+  name        = "${var.project}-NATGW-${random_string.id.result}"
+  description = "demo NATGW for terraform"
+  spec        = "1"
+  vpc_id      = flexibleengine_vpc_v1.vpc.id
+  subnet_id   = flexibleengine_networking_network_v2.front_net.id
+}
+
+#Add SNAT rule for Frontend subnet
+resource "flexibleengine_nat_snat_rule_v2" "snat_1" {
+  depends_on = [time_sleep.wait_for_vpc]  
+  nat_gateway_id = flexibleengine_nat_gateway_v2.nat_1.id
+  floating_ip_id = flexibleengine_vpc_eip_v1.eip_natgw.id
+  subnet_id      = flexibleengine_networking_network_v2.front_net.id
+}
+
+#Add SNAT rule for Backend subnet
+resource "flexibleengine_nat_snat_rule_v2" "snat_2" {
+  depends_on = [time_sleep.wait_for_vpc]  
+  nat_gateway_id = flexibleengine_nat_gateway_v2.nat_1.id
+  floating_ip_id = flexibleengine_vpc_eip_v1.eip_natgw.id
+  subnet_id      = flexibleengine_networking_network_v2.back_net.id
+}
+
 # Create an Elastic Cloud Server resource
 # Create an Elastic Cloud Server resource
 # resource "flexibleengine_compute_instance_v2" "test-server" {
