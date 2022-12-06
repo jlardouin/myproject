54c11e7135fa5d80ea71089771e41d35f8cfd92d
jlardouin <jlardouin@gmail.com>
2022-12-06 12:18:08 +0000

    Big commit

--- main.tf
+++ main.tf
@@ -1,13 +1,62 @@
 # main.tf is the main source code
 
-# Create an Elastic Cloud Server resource
-resource "flexibleengine_compute_instance_v2" "test-server" {
-  name        = "githubactions-server"
-  image_name  = "OBS Ubuntu 20.04"
-  flavor_name = "s6.small.1"
-  key_pair    = "key-jla"
-  security_groups = ["default"]
-  network {
-    uuid = "5dea92aa-0d97-443c-8e03-a2f566bc6cc6"
-  }
+# Create Virtual Private Cloud
+resource "flexibleengine_vpc_v1" "vpc" {
+  name = "${var.project}-vpc-${random_string.id.result}"
+  cidr = "${var.vpc_cidr}"
 }
+
+# Create Frontend network inside the VPC
+resource "flexibleengine_networking_network_v2" "front_net" {
+  name           = "${var.project}-front_net-${random_string.id.result}"
+  admin_state_up = "true"
+}
+
+# Create Backend network inside the VPC
+resource "flexibleengine_networking_network_v2" "back_net" {
+  name           = "${var.project}-back_net-${random_string.id.result}"
+  admin_state_up = "true"
+}
+
+# Create Frontend subnet inside the network
+resource "flexibleengine_networking_subnet_v2" "front_subnet" {
+  name            = "${var.project}-front_subnet-${random_string.id.result}"
+  cidr            = "${var.front_subnet_cidr}"
+  network_id      = flexibleengine_networking_network_v2.front_net.id
+  gateway_ip      = "${var.front_gateway_ip}"
+  dns_nameservers = ["100.125.0.41", "100.126.0.41"]
+}
+
+# Create Backend subnet inside the network
+resource "flexibleengine_networking_subnet_v2" "back_subnet" {
+  name            = "${var.project}-back_subnet-${random_string.id.result}"
+  cidr            = "${var.back_subnet_cidr}"
+  network_id      = flexibleengine_networking_network_v2.back_net.id
+  gateway_ip      = "${var.back_gateway_ip}"
+  dns_nameservers = ["100.125.0.41", "100.126.0.41"]
+}
+
+# Create Router interface for Frontend Network
+resource "flexibleengine_networking_router_interface_v2" "front_router_interface" {
+  router_id = flexibleengine_vpc_v1.vpc.id
+  subnet_id = flexibleengine_networking_subnet_v2.front_subnet.id
+}
+
+# Create Router interface for Backend Network
+resource "flexibleengine_networking_router_interface_v2" "back_router_interface" {
+  router_id = flexibleengine_vpc_v1.vpc.id
+  subnet_id = flexibleengine_networking_subnet_v2.back_subnet.id
+}
+
+# Create an Elastic Cloud Server resource
+# Create an Elastic Cloud Server resource
+# resource "flexibleengine_compute_instance_v2" "test-server" {
+#   name        = "githubactions-server"
+#   image_name  = "OBS Ubuntu 20.04"
+#   flavor_name = "s6.small.1"
+#   key_pair    = "key-jla"
+#   security_groups = ["default"]
+#   network {
+#     uuid = "5dea92aa-0d97-443c-8e03-a2f566bc6cc6"
+#   }
+# }
--- variables.tf
+++ variables.tf
@@ -10,3 +10,39 @@ variable "tenant_name" {
   default = "eu-west-0_jla"
   description = "tenant name of the domain to be refered"
 }
+variable "project" {
+  default = "demo"
+  description = "Short descriptive, readable label of the project you are working on. Is utilized as a part of resource names."
+}
+
+variable "vpc_cidr" {
+  default = "192.168.0.0/16"
+  description = "VPC CIDR."
+}
+
+variable "front_subnet_cidr" {
+  default = "192.168.1.0/24"
+  description = "Subnet CIDR."
+}
+
+variable "back_subnet_cidr" {
+  default = "192.168.2.0/24"
+  description = "Subnet CIDR."
+}
+
+variable "front_gateway_ip" {
+  default = "192.168.1.1"
+  description = "Subnet gateway IP."
+}
+
+variable "back_gateway_ip" {
+  default = "192.168.2.1"
+  description = "Subnet gateway IP."
+}
+
+# ID String for resources
+resource "random_string" "id" {
+  length  = 4
+  special = false
+  upper   = false
+}
